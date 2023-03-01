801490850e2ff5b245ee1857e9ab7dfe90b0cd0d
Kinyabundi <kinyachristine14@gmail.com>
2023-03-01 16:27:41 +0300

    Added the homepage

--- agritrcae_client/src/pages/index.tsx
+++ agritrcae_client/src/pages/index.tsx
@@ -1,10 +1,262 @@
-import MainLayout from "@/layouts/MainLayout";
-import { NextPageWithLayout } from "@/types/Layout";
-
-const Home: NextPageWithLayout = () => {
-  return <div>Home</div>;
+import {
+  Box,
+  Icon,
+  useColorModeValue,
+  createIcon,
+  chakra,
+  Image,
+} from "@chakra-ui/react";
+const Home = () => {
+  const bg = useColorModeValue("white", "gray.800");
+  return (
+    <Box pos="relative" overflow="hidden" bg={bg} mt={10}>
+      <Box maxW="7xl" mx="auto">
+        <Box
+          pos="relative"
+          pb={{
+            base: 8,
+            sm: 16,
+            md: 20,
+            lg: 28,
+            xl: 32,
+          }}
+          maxW={{
+            lg: "2xl",
+          }}
+          w={{
+            lg: "full",
+          }}
+          zIndex={1}
+          bg={bg}
+          border="solid 1px transparent"
+        >
+          <Icon
+            display={{
+              base: "none",
+              lg: "block",
+            }}
+            position="absolute"
+            right={0}
+            top={0}
+            bottom={0}
+            h="full"
+            w={48}
+            color={bg}
+            transform="translateX(50%)"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </Icon>
+          <Box
+            mx="auto"
+            maxW={{
+              base: "7xl",
+            }}
+            px={{
+              base: 4,
+              sm: 6,
+              lg: 8,
+            }}
+            mt={{
+              base: 10,
+              sm: 12,
+              md: 16,
+              lg: 20,
+              xl: 28,
+            }}
+          >
+            <Box
+              w="full"
+              textAlign={{
+                sm: "center",
+                lg: "left",
+              }}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <chakra.h1
+                fontSize={{
+                  base: "4xl",
+                  sm: "5xl",
+                  md: "6xl",
+                }}
+                letterSpacing="tight"
+                lineHeight="short"
+                fontWeight="extrabold"
+                color="gray.900"
+                _dark={{
+                  color: "white",
+                }}
+              >
+                <chakra.span
+                  display={{
+                    base: "block",
+                    xl: "inline",
+                  }}
+                >
+                  Data to enrich your{" "}
+                </chakra.span>
+                <chakra.span
+                  display={{
+                    base: "block",
+                    xl: "inline",
+                  }}
+                  color="brand.600"
+                  _dark={{
+                    color: "brand.400",
+                  }}
+                >
+                  online business
+                </chakra.span>
+              </chakra.h1>
+              <chakra.p
+                mt={{
+                  base: 3,
+                  sm: 5,
+                  md: 5,
+                }}
+                fontSize={{
+                  sm: "lg",
+                  md: "xl",
+                }}
+                maxW={{
+                  sm: "xl",
+                }}
+                mx={{
+                  sm: "auto",
+                  lg: 0,
+                }}
+                color="gray.500"
+              >
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua.
+              </chakra.p>
+              <Box
+                mt={{
+                  base: 5,
+                  sm: 8,
+                }}
+                display={{
+                  sm: "flex",
+                }}
+                justifyContent={{
+                  sm: "center",
+                  lg: "start",
+                }}
+                fontWeight="extrabold"
+                fontFamily="fantasy"
+              >
+                <Box rounded="full" shadow="md">
+                  <chakra.a
+                    w="full"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    border="solid 1px transparent"
+                    fontSize={{
+                      base: "md",
+                      md: "lg",
+                    }}
+                    rounded="md"
+                    color="white"
+                    bg="black"
+                    _hover={{
+                      bg: "brand.700",
+                    }}
+                    px={{
+                      base: 8,
+                      md: 10,
+                    }}
+                    py={{
+                      base: 3,
+                      md: 4,
+                    }}
+                    cursor="pointer"
+                  >
+                    Get started
+                  </chakra.a>
+                </Box>
+                <Box mt={[3, 0]} ml={[null, 3]}>
+                  <chakra.a
+                    w="full"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    px={{
+                      base: 8,
+                      md: 10,
+                    }}
+                    py={{
+                      base: 3,
+                      md: 4,
+                    }}
+                    border="solid 1px transparent"
+                    fontSize={{
+                      base: "md",
+                      md: "lg",
+                    }}
+                    rounded="md"
+                    color="brand.700"
+                    bg="brand.100"
+                    _hover={{
+                      bg: "brand.200",
+                    }}
+                    cursor="pointer"
+                  >
+                    Live demo
+                  </chakra.a>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        position={{
+          lg: "absolute",
+        }}
+        top={{
+          lg: 0,
+        }}
+        bottom={{
+          lg: 0,
+        }}
+        right={{
+          lg: 0,
+        }}
+        w={{
+          lg: "50%",
+        }}
+        border="solid 1px transparent"
+      >
+        <Image
+          h={[56, 72, 96, "full"]}
+          w="full"
+          fit="cover"
+          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          alt=""
+          loading="lazy"
+        />
+      </Box>
+    </Box>
+  );
 };
 
-Home.getLayout = (page) => <MainLayout>{page}</MainLayout>;
+const Arrow = createIcon({
+  displayName: "Arrow",
+  viewBox: "0 0 72 24",
+  path: (
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0.600904 7.08166C0.764293 6.8879 1.01492 6.79004 1.26654 6.82177C2.83216 7.01918 5.20326 7.24581 7.54543 7.23964C9.92491 7.23338 12.1351 6.98464 13.4704 6.32142C13.84 6.13785 14.2885 6.28805 14.4722 6.65692C14.6559 7.02578 14.5052 7.47362 14.1356 7.6572C12.4625 8.48822 9.94063 8.72541 7.54852 8.7317C5.67514 8.73663 3.79547 8.5985 2.29921 8.44247C2.80955 9.59638 3.50943 10.6396 4.24665 11.7384C4.39435 11.9585 4.54354 12.1809 4.69301 12.4068C5.79543 14.0733 6.88128 15.8995 7.1179 18.2636C7.15893 18.6735 6.85928 19.0393 6.4486 19.0805C6.03792 19.1217 5.67174 18.8227 5.6307 18.4128C5.43271 16.4346 4.52957 14.868 3.4457 13.2296C3.3058 13.0181 3.16221 12.8046 3.01684 12.5885C2.05899 11.1646 1.02372 9.62564 0.457909 7.78069C0.383671 7.53862 0.437515 7.27541 0.600904 7.08166ZM5.52039 10.2248C5.77662 9.90161 6.24663 9.84687 6.57018 10.1025C16.4834 17.9344 29.9158 22.4064 42.0781 21.4773C54.1988 20.5514 65.0339 14.2748 69.9746 0.584299C70.1145 0.196597 70.5427 -0.0046455 70.931 0.134813C71.3193 0.274276 71.5206 0.70162 71.3807 1.08932C66.2105 15.4159 54.8056 22.0014 42.1913 22.965C29.6185 23.9254 15.8207 19.3142 5.64226 11.2727C5.31871 11.0171 5.26415 10.5479 5.52039 10.2248Z"
+      fill="currentColor"
+    />
+  ),
+});
 
 export default Home;
