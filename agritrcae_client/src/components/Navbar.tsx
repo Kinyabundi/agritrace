f340ed0d6aaa23908cb2f363e99fb47ecfdb47b9
Kinyabundi <kinyachristine14@gmail.com>
2023-03-03 09:36:07 +0300

    add auth page

---
+++ agritrcae_client/src/components/Navbar.tsx
@@ -0,0 +1,168 @@
+import { NextPageWithLayout } from "@/types/Layout";
+import {
+  useColorModeValue,
+  useDisclosure,
+  chakra,
+  Flex,
+  HStack,
+  IconButton,
+  VStack,
+  CloseButton,
+  Button,
+  VisuallyHidden,
+  Avatar,
+  Box,
+} from "@chakra-ui/react";
+import React from "react";
+import {
+  AiOutlineMenu,
+  AiFillHome,
+  AiOutlineInbox,
+  AiFillBell,
+} from "react-icons/ai";
+import { BsFillCameraVideoFill, BsPlus } from "react-icons/bs";
+
+const Navbar = () => {
+  const bg = useColorModeValue("white", "gray.800");
+  const mobileNav = useDisclosure();
+  return (
+    <React.Fragment>
+      <chakra.header
+        bg={bg}
+        w="full"
+        px={{
+          base: 2,
+          sm: 4,
+        }}
+        py={4}
+        shadow="md"
+      >
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <HStack display="flex" spacing={3} alignItems="center">
+            <Box
+              display={{
+                base: "inline-flex",
+                md: "none",
+              }}
+            >
+              <IconButton
+                display={{
+                  base: "flex",
+                  md: "none",
+                }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color="gray.800"
+                _dark={{
+                  color: "inherit",
+                }}
+                variant="ghost"
+                icon={<AiOutlineMenu />}
+                onClick={mobileNav.onOpen}
+              />
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? "flex" : "none"}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg={bg}
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
+              >
+                <CloseButton
+                  aria-label="Close menu"
+                  justifySelf="self-start"
+                  onClick={mobileNav.onClose}
+                />
+                <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
+                  Dashboard
+                </Button>
+                <Button
+                  w="full"
+                  variant="solid"
+                  colorScheme="brand"
+                  leftIcon={<AiOutlineInbox />}
+                >
+                  Inbox
+                </Button>
+                <Button
+                  w="full"
+                  variant="ghost"
+                  leftIcon={<BsFillCameraVideoFill />}
+                >
+                  Videos
+                </Button>
+              </VStack>
+            </Box>
+            <chakra.h1
+              title="Choc Home Page"
+              display="flex"
+              alignItems="center"
+              fontSize={{
+                base: "4xl",
+                sm: "3xl",
+                md: "2xl",
+              }}
+                fontWeight="bold"
+                // color="blue"
+            >
+              AgriTrace
+              <VisuallyHidden>Choc</VisuallyHidden>
+            </chakra.h1>
+
+            <HStack
+              spacing={3}
+              display={{
+                base: "none",
+                md: "inline-flex",
+              }}
+            >
+             
+            </HStack>
+          </HStack>
+          <HStack
+            spacing={3}
+            display={mobileNav.isOpen ? "none" : "flex"}
+            alignItems="center"
+          >
+            <Button colorScheme="blue">
+              Join Us
+            </Button>
+
+            <chakra.a
+              p={3}
+              color="gray.800"
+              _dark={{
+                color: "inherit",
+              }}
+              rounded="sm"
+              _hover={{
+                color: "gray.800",
+                _dark: {
+                  color: "gray.600",
+                },
+              }}
+            >
+              {/* <AiFillBell />
+              <VisuallyHidden>Notifications</VisuallyHidden> */}
+            </chakra.a>
+
+            {/* <Avatar
+              size="sm"
+              name="Dan Abrahmov"
+              src="https://bit.ly/dan-abramov"
+            /> */}
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
